62cc573f354fba79acb0ce0ca18bcf25603097d4
anand-vyas-gg <anand.vyas@globalgarner.com>
2024-05-15 14:26:11 +0530

    postcss fixed and changes in overall setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./lib/**/*.{html,js}", "./src/**/*.{html,js}"],
+    content: [
+        "./**/*.{html,jsx}",
+        "./lib/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {},
     },
--- testing.jsx
+++ testing.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./lib/styles.css";
 
 createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <p>Hello</p>
+        <p className="text-4xl">Hello</p>
     </React.StrictMode>
 );
